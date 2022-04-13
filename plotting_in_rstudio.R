d9eed22e2eff2378e48dcc08481ba37187517c48
nevasepracheta <97940662+nevasepracheta@users.noreply.github.com>
2022-04-13 11:15:09 +0530

    Update plotting_in_rstudio.R

--- plotting_in_rstudio.R
+++ plotting_in_rstudio.R
@@ -6,7 +6,7 @@ head(mtcars, 5)
 
 #load ggplot package; if package is missing use Tools-> Install packages option in RStudio to install the missing package
 library(ggplot2)
-# create a scatterplot of displacement (disp) and miles per gallon (mpg)
+# Create a Scatterplot of displacement (disp) and miles per gallon (mpg); geom_point() for Scatterplot
 ggplot(aes(x=disp,y=mpg,),data=mtcars)+geom_point()
 
 #Add plot title
@@ -16,12 +16,13 @@ ggplot(aes(x=disp, y=mpg,), data=mtcars)+geom_point()+ggtitle("Displacement vs M
 
 #make vs(Engine variable of mtcars) a factor
 mtcars$vs <- as.factor(mtcars$vs)
-#create boxplot of the distribution for v-shaped and straight engine
+
+#Create Boxplot of the distribution for v-shaped and straight engine; geom_boxplot() for Box Plot
 ggplot(aes(x=vs, y=mpg), data = mtcars)+geom_boxplot()
 
 #add color to boxplot
 ggplot(aes(x=vs, y=mpg, fill=vs), data = mtcars) + geom_boxplot(alpha=0.3)+theme(legend.position="none")
 
-#Create a Histogram of variable weight wt
+#Create a Histogram of variable weight wt; geom_histogram() for Histogram
 ggplot(aes(x=wt),data=mtcars)+geom_histogram(binwidth=0.5)
        
